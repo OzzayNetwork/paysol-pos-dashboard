b096d5d5448a9cea4ad51341588a28ade692ae89
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-03-16 14:40:40 +0300

    kelvin: date range picker added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -68,8 +68,40 @@ $(window).on('load', function() {
     })
 
 });
+$(document).ready(function(){
+    // date range picker
+    $(function() {
+
+        var start = moment().subtract(29, 'days');
+        var end = moment();
+
+        function cb(start, end) {
+            $('#reportrange .selected-date').html(start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
+        }
+
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
+        cb(start, end);
+
+    });
+    // date range picker
+})
 
 $(document).ready(function() {
+
+    
+
     $('.selectpicker').selectpicker();
     $('.selectpicker').selectpicker('render')
 });
