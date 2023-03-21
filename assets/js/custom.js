4d0c4baa0d28f28a2e8f387b183b7763fe5c0fc2
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-03-21 16:35:55 +0300

    kelvin: settings images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -140,3 +140,33 @@ $(document).ready(function(){
     });
 })
 
+$(document).ready(function(){
+    // alert("setting date")
+    function getDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+      
+        if(dd<10) {
+            dd = '0'+dd
+        } 
+      
+        if(mm<10) {
+            mm = '0'+mm
+        } 
+      
+        today = yyyy + '-' + mm + '-' + dd;
+        // today = 0+':'+dd+'/'+mm+'/'+yyyy;
+        console.log(today);
+        // document.getElementById("example-date-input").value = today;
+        $('#example-date-input').val(today)
+       
+      }
+      
+      
+      window.onload = function() {
+        getDate();
+      };
+})
+
